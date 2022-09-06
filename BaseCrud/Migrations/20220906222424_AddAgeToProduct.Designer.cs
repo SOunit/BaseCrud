7f125b1e312cfcc4c8c688f7941e2d271da74e37
SOunit <workbookwork7@gmail.com>
2022-09-06 15:28:13 -0700

    add age to product

--- BaseCrud/Migrations/20220906222424_AddAgeToProduct.Designer.cs
+++ BaseCrud/Migrations/20220906222424_AddAgeToProduct.Designer.cs
@@ -3,6 +3,7 @@ using BaseCrud.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BaseCrud.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220906222424_AddAgeToProduct")]
+    partial class AddAgeToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BaseCrud/Models/Product.cs
+++ BaseCrud/Models/Product.cs
@@ -8,5 +8,7 @@ namespace BaseCrud.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
